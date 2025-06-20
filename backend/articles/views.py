6dc6c430e51de1046f637b7e66f88be32b2cd1fb
Chris Klemz <crklemz@gmail.com>
2025-06-20 14:11:03 -0500

    feat: create articles viewset

--- backend/articles/views.py
+++ backend/articles/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Article
+from .serializers import ArticleSerializer
+
+class ArticleViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = ArticleSerializer
+
+    def get_queryset(self):
+        site_slug = self.request.query_params.get('site')
+        if site_slug:
+            return Article.objects.filter(site__slug=site_slug, status='approved')
+        return Article.objects.filter(status='approved')
