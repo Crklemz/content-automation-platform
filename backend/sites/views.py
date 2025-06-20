a7aba9b15ad9b05786bca68e2a709df0a676c724
Chris Klemz <crklemz@gmail.com>
2025-06-20 14:11:33 -0500

    feat: create sites viewset

--- backend/sites/views.py
+++ backend/sites/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Site
+from .serializers import SiteSerializer
+
+class SiteViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Site.objects.all()
+    serializer_class = SiteSerializer
+    lookup_field = 'slug'
