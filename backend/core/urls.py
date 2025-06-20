7d1a4a4bf5928d1f5e3961c8814817dde2e47bf1
Chris Klemz <crklemz@gmail.com>
2025-06-20 14:12:50 -0500

    feat: register api routes

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from sites.views import SiteViewSet
+from articles.views import ArticleViewSet
+
+router = DefaultRouter()
+router.register(r'sites', SiteViewSet, basename='site')
+router.register(r'articles', ArticleViewSet, basename='article')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
