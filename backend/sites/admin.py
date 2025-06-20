7dc2d1048475c57e51cd47041dea0695d8ccc902
Chris Klemz <crklemz@gmail.com>
2025-06-20 13:08:05 -0500

    feat: register site model

--- backend/sites/admin.py
+++ backend/sites/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Site
+
+@admin.register(Site)
+class SiteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'topic')
+    search_fields = ('title', 'topic')
