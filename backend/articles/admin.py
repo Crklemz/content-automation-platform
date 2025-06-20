077cb6bf3c99a15ef1678642749e6c8fcfe19821
Chris Klemz <crklemz@gmail.com>
2025-06-20 13:08:43 -0500

    feat: register article model

--- backend/articles/admin.py
+++ backend/articles/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Article
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'site', 'status', 'created_at')
+    list_filter = ('status', 'site')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    readonly_fields = ('created_at',)
