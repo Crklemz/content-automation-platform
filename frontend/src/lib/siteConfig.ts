98b189e8cc38558916b2e9ab2678c7cbb403c83c
Chris Klemz <crklemz@gmail.com>
2025-06-20 14:46:50 -0500

    feat: add temp site config

---
+++ frontend/src/lib/siteConfig.ts
@@ -0,0 +1,14 @@
+export const siteConfig = [
+  {
+    slug: "ai-skills",
+    title: "Stay Relevant with AI",
+    topic: "Artificial Intelligence Skills and Tools",
+    logo: "/images/logos/ai-skills.svg",
+    colors: {
+      primary: "#1a73e8",
+      secondary: "#185abc",
+      tertiary: "#d2e3fc",
+      quaternary: "#f1f3f4",
+    },
+  },
+];
